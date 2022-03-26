960904d78b27b5e8bccf4b1393e5183b2ab6b985
spamcage <service@spamcage.com>
2022-03-26 10:56:41 +0800

    f

--- data.js
+++ data.js
@@ -46,7 +46,7 @@ var data = [
     ],
     [
         'mail.yandex.com',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Yandex_Mail_wordmark.svg/250px-Yandex_Mail_wordmark.svg.png',
+        'https://upload.wikimedia.org/wikipedia/commons/c/cf/Yandex_2021.svg',
         'Yandex.Mail (Russian: Яндекс Почта; stylised as Yandex.Mail) is a Russian free email service developed by Yandex. It was launched on June 26, 2000,[1][2] and is one of the three largest email services in Runet (along with Gmail and Mail.ru).[3] The service uses automatic spam filtering, checks for viruses using antivirus software from Dr.Web, and an email translator'
     ],
     [
